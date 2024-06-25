a9826748e62ef7f9156c6518e99b90fc85461acd
Mark Wang <armstrong.wang2000@gmail.com>
2024-06-25 16:33:17 -0600

    Model

---
+++ snippets/models.py
@@ -0,0 +1,27 @@
+from typing import List, Tuple
+
+from django.db import models
+from pygments.lexers import get_all_lexers
+from pygments.styles import get_all_styles
+
+# Define type aliases for better readability
+LexerInfo = Tuple[str, Tuple[str, ...], Tuple[str, ...], Tuple[str, ...]]
+LexerTuple = Tuple[str, str]
+StyleTuple = Tuple[str, str]
+
+# Create your models here.
+LEXERS: List[LexerInfo] = [item for item in get_all_lexers() if item[1]]
+LANGUAGE_CHOICES: List[LexerTuple] = sorted([(item[1][0], item[0]) for item in LEXERS])
+STYLE_CHOICES: List[StyleTuple] = sorted([(item, item) for item in get_all_styles()])
+
+
+class Snippet(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, blank=True, default='')
+    code = models.TextField()
+    linenos = models.BooleanField(default=False)
+    language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
+    style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
+
+    class Meta:
+        ordering = ['created']
